0ed50dd50727166da8ae869f38bd43e5dc610b72
gouravfreedom@gmail.com <qwerty@123>
2018-12-06 21:57:38 +0530

    Data

---
+++ DFS/Controls/CustomPicker.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace DFS
+{
+    public class CustomPicker : Picker
+    {
+        public static readonly BindableProperty ColorProperty =
+            BindableProperty.Create(nameof(BorderColor), typeof(string), typeof(CustomPicker), string.Empty);
+
+        public string BorderColor
+        {
+            get { return (string)GetValue(ColorProperty); }
+            set { SetValue(ColorProperty, value); }
+        }
+    }
+}
---
+++ iOS/CustomPickerRenderer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using DFS;
+using DFS.iOS.Controls;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(CustomPicker), typeof(CustomPickerRenderer))]
+namespace DFS.iOS.Controls
+{
+    public class CustomPickerRenderer : PickerRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Picker> e)
+        {
+            base.OnElementChanged(e);
+
+            var element = (CustomPicker)this.Element;
+
+            if (this.Control != null && this.Element != null )
+            {
+                Control.BorderStyle = UITextBorderStyle.RoundedRect;
+                Control.Layer.CornerRadius = 8.0f;
+                Control.Layer.MasksToBounds = true;
+                Control.ClipsToBounds = true;
+
+                Control.Layer.BorderWidth = 1;
+
+                if(element.BorderColor == "RED"){
+                    Control.Layer.BorderColor = UIColor.Red.CGColor;
+                }else{
+                    Control.Layer.BorderColor = UIColor.LightGray.CGColor;
+                }
+
+            }
+        }
+    }
+}
